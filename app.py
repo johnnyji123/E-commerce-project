0f39d360cbce57732d969619462ed900388582a3
Johnny Ji <jj_johnny97@hotmail.com>
2023-12-17 09:33:16 +0000

    created products table in my database

--- app.py
+++ app.py
@@ -14,3 +14,22 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 
+# table products
+
+
+values = [
+        ("Marco resin 1/4", 380,  1),
+        ("Aokji resin 1/6", 420, 1),
+        ("Blackebeard resin sd", 260, 1)
+    ]
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def products_page():
+    return render_template("products.html")
+
+
+if __name__ == ("__main__"):
+    app.run(debug= True, use_reloader = False)
